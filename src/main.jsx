7c521a7ea0b5520da1234f48a03afd383fcdbadd
Danyil <liberlay555@gmail.com>
2025-04-04 14:54:08 +0300

    Change main.jsx layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
 import { createRoot } from 'react-dom/client'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App.jsx'
 
@@ -10,7 +11,9 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </StrictMode>
 )
